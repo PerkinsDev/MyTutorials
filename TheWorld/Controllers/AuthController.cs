040fb38d1c111099d8572e343e50d86f9bf80851
Andy Perkins <andyperkins1978@gmail.com>
2016-10-17 11:53:44 -0400

    Pre-update. fix tag helper intellisense???

---
+++ TheWorld/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheWorld.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");  // Redirect to Trips page so not asked to login again if logged in
+            }
+
+
+            return View();
+        }
+    }
+}
