3be74c4f6a6a1352949ee8a81a4b29efd788d23d
Andy Perkins <andyperkins1978@gmail.com>
2016-10-27 14:34:30 -0400

    angular form validation start

---
+++ TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,10 @@
+﻿// app-trips.js
+// take out of global scope with immediately invoking function expression
+(function () {
+
+    "use strict";
+    // angular.module with 2 params creates the actual module which we use in other js files
+    // where we define a packet of code for angular. Creates a module that we can then specify in the trips where to get this code
+    angular.module("app-trips", []);     // empty array is how dependencies are managed
+
+})();
---
+++ TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,40 @@
+﻿//tripsController.js
+(function() {
+
+    "use strict";
+
+    // angular.module with 1 param gets the existing module created before
+    // Create a controller, it's name, and the function that will actually hold the body of the code
+    // returns an object on which we can add our controller
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    // function that is the actual tripsController (body of code)
+    function tripsController() {
+        
+     	// expose some data from the controller (could use this which calls and return a new tripController and returns a prototypical class)
+        // declaing this as vm to be more descriptive as a view model
+        var vm = this;
+
+		// construct an array of objects called vm.trips and add properties
+        vm.trips = [
+            {
+                name: "US Trip",
+                created: new Date()
+            },
+            {
+                name: "World Trip",
+                created: new Date()
+            }
+        ];
+
+        // represent an object that is going to accept all the data about a new trip
+        vm.newTrip = {};   // object literal for obj with no props
+
+        //called when user click on the add trip btn. set with ng-submit on form tag with function name
+        vm.addTrip = function() {
+            alert(vm.newTrip.name);
+        };
+    }
+
+})();
