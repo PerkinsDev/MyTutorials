1b8078379be180700deab5d0fc3f8411afcd2dce
Andy Perkins <andyperkins1978@gmail.com>
2016-11-06 18:06:32 -0500

    Add Angular Routing

--- TheWorld/wwwroot/js/app-trips.js
+++ TheWorld/wwwroot/js/app-trips.js
@@ -5,6 +5,7 @@
     "use strict";
     // angular.module with 2 params creates the actual module which we use in other js files
     // where we define a packet of code for angular. Creates a module that we can then specify in the trips where to get this code
-    angular.module("app-trips", []);     // empty array is how dependencies are managed
+    // need to tell this main module to expect and inject our simple controls.js file to access our custom directives /array is how dependencies are managed
+    angular.module("app-trips", ["simpleControls"]); 
 
 })();
---
+++ TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,20 @@
+﻿// simpleControls.js
+(function() {
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    // directive returns an object and can define the properties here
+    function waitCursor() {
+        return {
+            // scope = a structre/obj we are binding our waitcurspor to. way to map our new attrib. over to the template
+            scope: {
+                show: "=displayWhen"   // names don't have to match but show is used in the waitCursor.html template as ng-show="show"
+            },
+            restrict: "E",
+            templateUrl: "/views/waitCursor.html"  // client side so it goes in wwwroot
+        };
+    }
+
+})();
